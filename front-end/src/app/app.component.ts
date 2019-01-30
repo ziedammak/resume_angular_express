720643436a37e11bd1cadce077c4c09314742ee8
ziedammak <41289114+ziedammak@users.noreply.github.com>
2019-01-30 06:47:06 +0100

    css

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -5,8 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   title=  'CV';
